3f175b767c788b4d1108c793bb7b61088985ad28
TeTeT Nimitz <tetet.nimitz@gmail.com>
2023-11-11 22:30:38 +0100

    Start of p4 mod

--- entry.lua
+++ entry.lua
@@ -1,18 +1,18 @@
-declare_plugin("Ship name by developer",
+declare_plugin("tetet_p4",
 {
 dirName		  = current_mod_path,
-displayName   = _("Ship name"),
-shortName	  =   "Short Ship name",
-version		  =   "Version number",
+displayName   = _("p4 Torpedo Boat),
+shortName	  =   "p4",
+version		  =   "2.9.0",
 state		  =   "installed",
-developerName =   "developer name",
-info		  =    _("info on the main screen on the icon"),
+developerName =   "TeTeT",
+info		  =    _("p4 Torpedo Boat"),
 encyclopedia_path = current_mod_path .. '/Encyclopedia',
 
 Skins = 
 {
 	 {     
-	     name  = "Ship name", 
+	     name  = "p4", 
 		 dir   = "Skins/1" 
 	 },
 },
@@ -21,9 +21,9 @@ Skins =
 
 mount_vfs_liveries_path (current_mod_path ..  "/Liveries")
 mount_vfs_model_path    (current_mod_path ..  "/Shapes")
-mount_vfs_texture_path	(current_mod_path ..  "/Textures/ShipTextures.zip") -- I use unique names to cut down on any errors and combatibility clashes with other mods
+mount_vfs_texture_path	(current_mod_path ..  "/Textures/p4.zip") -- I use unique names to cut down on any errors and combatibility clashes with other mods
 
-dofile(current_mod_path..'/Weapons/Weapon.lua') -- any custom weapons the mod has
+-- dofile(current_mod_path..'/Weapons/Weapon.lua') -- any custom weapons the mod has
 dofile(current_mod_path .."/Database/db_ships.lua")
 
 
